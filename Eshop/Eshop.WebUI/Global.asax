2b8c88d85ba12a00ae75ac167e069e6df8a605ee
Klajdi <Klajdi@KLAJDI-PC>
2018-05-20 17:16:45 +0300

    Solution setup

---
+++ Eshop/Eshop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eshop.WebUI.MvcApplication" Language="C#" %>
---
+++ Eshop/Eshop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eshop.WebUI.Startup))]
+namespace Eshop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
